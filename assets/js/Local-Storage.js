201b186b712bdaddfd2554da4a51f850d45c07ef
Daniel V <main@dvasquez4155.com>
2020-02-05 16:36:01 -0800

    $message

--- assets/js/Local-Storage.js
+++ assets/js/Local-Storage.js
@@ -2,10 +2,10 @@ var values = getValues();
 function getValues() {
     var retrievedObject = localStorage.getItem('values');
     if (retrievedObject == null) {
-        var emptyValues = new Array(9);
-        for (var i = 0; i < emptyValues.length; i++) {
-            emptyValues[i] = "";
-        }
+        var emptyValues = [];
+        // for (var i = 0; i < emptyValues.length; i++) {
+        //     emptyValues[i] = "";
+        // }
         return emptyValues;
     }
     return JSON.parse(retrievedObject);
